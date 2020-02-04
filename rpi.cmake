3e4b576d48394f17265304317e64dd6770f31414
Patrick Hurley <phurley@gmail.com>
2020-02-04 12:09:37 -0500

    Cleanup make to be more generic, put debug flag into config file to support debug streams (expensive timewise)

--- rpi.cmake
+++ rpi.cmake
@@ -3,11 +3,14 @@ SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_VERSION 1)
 
 # Define the cross compiler locations
-SET(CMAKE_C_COMPILER /Users/phurley/wpilib/raspbian10/bin/arm-raspbian10-linux-gnueabihf-gcc)
-SET(CMAKE_CXX_COMPILER /Users/phurley/wpilib/raspbian10/bin/arm-raspbian10-linux-gnueabihf-g++)
-
-# Define the sysroot path for the RaspberryPi distribution in our tools folder 
-SET(CMAKE_FIND_ROOT_PATH /Users/phurley/wpilib/raspbian10/sys-root)
+if(DEFINED ENV{ARM_HOME})
+  SET(CMAKE_C_COMPILER $ENV{ARM_HOME}/bin/arm-raspbian10-linux-gnueabihf-gcc)
+  SET(CMAKE_CXX_COMPILER $ENV{ARM_HOME}/bin/arm-raspbian10-linux-gnueabihf-g++)
+  SET(CMAKE_FIND_ROOT_PATH $ENV{ARM_HOME}/sys-root)
+else()
+  # I wrote it, I get to pick the defaults
+  MESSAGE(FATAL_ERROR "Please set an environement variable named ARM_HOME\nLinux:\nexport ARM_HOME=/Users/phurley/wpilib/raspbian10\nWindows:\nSET ARM_HOME=c:\\wpilib\\raspbian10\n before generating the project")
+endif()
 
 # Use our definitions for compiler tools
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
@@ -17,8 +20,9 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
-#add_compile_options(-Wall std=c++17 -Wl,--unresolved-symbols=ignore-in-shared-libs)
+
 add_link_options(-Wl,--unresolved-symbols=ignore-in-shared-libs)
+add_compile_options(-Wall -Wno-psabi -std=c++2a)
 
 set(CMAKE_BUILD_TYPE Release)
 
