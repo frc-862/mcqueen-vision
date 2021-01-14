bafd0fa72bf228e4810c27a597fa86d16ddf15f7
Bobev64 <68569762+Bobev64@users.noreply.github.com>
2021-01-13 21:53:39 -0500

    SC2-34 - Rough outline of network table - default value output ONLY

--- Pipelines/src/main/java/pipelines/PowerPortPipeline.java
+++ Pipelines/src/main/java/pipelines/PowerPortPipeline.java
@@ -25,6 +25,26 @@ public class PowerPortPipeline implements LightningVisionPipeline {
     @Override
     public void log() {
         // Log to Network Table `ntab` here.
+
+        ntab.getEntry("FieldOfViewVert").setDouble(43.30);
+        ntab.getEntry("FieldOfViewHoriz").setDouble(70.42);
+        ntab.getEntry("TargetHeightRatio").setDouble(1.0);
+        ntab.getEntry("TargetRatioThreshold").setDouble(100.0);
+        ntab.getEntry("TargetDistanceThreshold").setDouble(0.5);
+        ntab.getEntry("TargetHeightAConstant").setDouble(0.0084);
+        ntab.getEntry("TargetHeightBConstant").setDouble(-1.4737);
+        ntab.getEntry("TargetHeightCConstant").setDouble(76.27);
+        ntab.getEntry("TargetRowAConstant").setDouble(-0.0056);
+        ntab.getEntry("TargetRowBConstant").setDouble(4.4264);
+        ntab.getEntry("TargetRowCConstant").setDouble(-832.33);
+
+        ntab.getEntry("VisionFound").setDouble(0);
         
+        ntab.getEntry("VisionX").setDouble(0);
+        ntab.getEntry("VisionY").setDouble(0);
+        ntab.getEntry("VisionHeight").setDouble(0);
+        ntab.getEntry("VisionDistance").setDouble(0);
+        ntab.getEntry("VisionAngle").setDouble(0);
+        ntab.getEntry("VisionDelay").setDouble(0.1);
     }  
 }
